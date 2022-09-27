bf2a6fc36d3d333fb8e2aa1a3fb5782b4e59e1ea
Abdiel Esteban Alfonso Chávez <isc19350558@gmail.com>
2022-09-27 10:51:29 -0500

    Codigos de respuestas HTTP

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,26 +1,26 @@
 const { request, response } = require("express")
 
 const rootMessage = (req = request,res = response)=>{
-    res.json({msg:"Hola"})
+    res.status(404).json({msg:"Hola"})
 }
 
 const hiMessage = (req = request,res = response)=>{
-    res.json({msg:"Hola"})
+    res.status(400).json({msg:"Hola"})
 }
 
 const byeMessage = (req = request,res = response)=>{
-    res.json({msg:"Adios mundo"})
+    res.status(401).json({msg:"Adios mundo"})
 }
 
 const postMessage = (req = request,res = response)=>{
-    res.json({msg:"Mensaje POST"})
+    res.status(403).json({msg:"Mensaje POST"})
 }
 
 const putMessage = (req = request,res = response)=>{
-    res.json({msg:"Mensaje PUT"})
+    res.status(405).json({msg:"Mensaje PUT"})
 }
 
 const deleteMessage = (req = request,res = response)=>{
-    res.json({msg:"Mensaje DELETE"})
+    res.status(406).json({msg:"Mensaje DELETE"})
 }
 module.exports = {rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage}
