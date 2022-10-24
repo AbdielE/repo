432c8ba3b9f6224a015a6a4e7c6c537295a14c47
Abdiel Esteban Alfonso Chávez <isc19350558@gmail.com>
2022-10-24 10:50:03 -0500

    Eliminar usuario

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,10 +1,12 @@
 const {Router} = require("express")
-const {getUsers, getUserbyID} = require("../controllers/usuarios")
+const {getUsers, getUserbyID, deleteUserbyID} = require("../controllers/usuarios")
 const router = Router()
 
-// http://localhost:4000/api/v1/usuarios/
+// http://localhost:4000/api/v1/usuarios/?id=1
 
 router.get("/",getUsers)
 router.get("/id/:id",getUserbyID)
+
+router.delete("/", deleteUserbyID)
 
 module.exports = router
