790bdad2b0bc4789f4b51b43e2b5121e3c74e236
Abdiel Esteban Alfonso Chávez <isc19350558@gmail.com>
2022-11-18 10:21:09 -0600

    Base de datos

---
+++ prueba.sql
@@ -0,0 +1,94 @@
+-- MariaDB dump 10.19  Distrib 10.6.7-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: prueba
+-- ------------------------------------------------------
+-- Server version	10.6.7-MariaDB-2ubuntu1.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `personajes`
+--
+
+DROP TABLE IF EXISTS `personajes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personajes` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(255) NOT NULL,
+  `Alias` varchar(50) NOT NULL,
+  `Tipo` varchar(8) NOT NULL,
+  `Organizacion` varchar(30) NOT NULL,
+  `ArmaP` varchar(50) NOT NULL,
+  `ArmaS` varchar(50) NOT NULL,
+  `Dispositivo` varchar(50) NOT NULL,
+  `Habilidad` varchar(70) NOT NULL,
+  `Activo` char(1) NOT NULL,
+  `Creado` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Modificado` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personajes`
+--
+
+LOCK TABLES `personajes` WRITE;
+/*!40000 ALTER TABLE `personajes` DISABLE KEYS */;
+INSERT INTO `personajes` VALUES (4,'Prueba2B','PruebaAB2 h','Defensor','GIGN','ap2','as2','granada','si2','S','2022-11-15 16:24:54','2022-11-15 16:24:54'),(5,'Prueba2B','PruebaAB2 hvc ','Defensor','GIGN','ap2','as2','granada','si2','S','2022-11-16 16:08:52','2022-11-16 16:08:52');
+/*!40000 ALTER TABLE `personajes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Usuario` varchar(255) NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Apellidos` varchar(255) NOT NULL,
+  `Edad` int(3) NOT NULL,
+  `Genero` char(1) DEFAULT NULL,
+  `Contrasena` varchar(255) NOT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL,
+  `Activo` char(1) NOT NULL,
+  `Creado` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Modificado` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (7,'pruebagmail.com','Abdiel E.','Alfonso C.',22,'M','1234','2001-08-07','S','2022-11-17 16:40:29','2022-11-18 16:11:26');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-18 10:17:12
